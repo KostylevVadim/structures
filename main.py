9fb4334978b28922846b3f6e04aa1b11ae44496d
Vadim Kostylev <vadik.kostylev@yandex.ru>
2023-09-19 22:02:23 +0300

    commit on master

--- main.py
+++ main.py
@@ -4,11 +4,3 @@ from my_types.stack import Stack
 from my_types.binary_tree import BinaryTree
 from my_types.graph import Graph
 
-tree = BinaryTree()
-
-tree.add(5)
-tree.add(1)
-tree.add(10)
-tree.add(12)
-tree.add(0)
-tree.go_with_high()
