bb70cbfa9b31b159d3b95081c43f2e11045d0ec6
Vadim Kostylev <vadik.kostylev@yandex.ru>
2023-09-22 14:51:12 +0300

    commit on master

--- my_types/cache.py
+++ my_types/cache.py
@@ -1,5 +1,47 @@
 from collections import Counter
 class Cache():
     def __init__(self, data: list, limit :int):
-        x = {data.count(data[i]): data[i] for i in range(len(data))}
-        print(x)
+        x = {data[i]:data.count(data[i]) for i in range(len(data))}
+        self.__cache = x
+        self.__limit = limit
+        
+        self.__cache = dict(sorted(x.items(), key=lambda item: item[1])[::-1][:min(self.__limit, len(data))])
+    
+    def __str__(self):
+        s = 'Cache can store '+str(self.__limit)+' elements\nNow stored: '+str(len(self.__cache))+'\nData:'
+        for key, value in self.__cache.items():
+            s+='\nData '+ str(key)+ ' requested '+ str(value)+ ' times'
+        return s
+        
+        
+
+    def add(self, data):
+        if data in self.__cache.keys():
+             self.__cache[data]+=1
+        else:
+            if len(self.__cache) < self.__limit:
+                self.__cache[data] = 1
+            else:
+                self.__cache.popitem()
+                self.__cache[data] = 1
+        x = self.__cache
+        self.__cache = dict(sorted(x.items(), key=lambda item: item[1])[::-1])
+
+    def get_most_requested(self):
+        x = max(self.__cache.values())
+        for key, value in self.__cache.items():
+            if value == x:
+                return key
+    def get_least_requested(self):
+        x = min(self.__cache.values())
+        for key, value in self.__cache.items():
+            if value == x:
+                return key
+
+
+
+
+
+    
+    
+        
